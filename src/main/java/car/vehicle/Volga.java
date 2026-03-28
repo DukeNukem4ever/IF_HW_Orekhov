0593246ec01eee5c15010811d19e52ae1f8726c1
dukenukem4ever <or._natali@mail.ru>
2026-03-28 23:44:35 +0300

    init commit

--- src/main/java/car/vehicle/Volga.java
+++ src/main/java/car/vehicle/Volga.java
@@ -3,9 +3,10 @@ package car.vehicle;
 import car.Car;
 
 public class Volga extends Car {
+
     public Volga(String name, String model, int price, int year, String transmission, String color, int avg_speed, String country) {
         super(name,
-                "Volga",
+                model,
                 price,
                 year,
                 transmission,
